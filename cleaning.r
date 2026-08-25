b2fa2a8b6058ee6d27ef6a22c75d95742572839d
Nathaniel Victor <nathanielvictor2004@gmail.com>
2026-08-25 13:46:35 +0100

    docs: Add comments to cleaning.r

--- cleaning.r
+++ cleaning.r
@@ -1,22 +1,26 @@
 library(tidyverse)
 library(janitor)
 
+# Concatenate all CSV files in the `data` directory together
 data <- list.files(path = "data", full.names = TRUE) |>
   read_csv() |>
+  # Clean the column names
   clean_names() |>
+  # Convert the `period` column to the last day of the corresponding month
   mutate(period = ceiling_date(my(period), unit = "month") - days(1))
 
+# Create separate table for new RTT pathways
 starting <- data |>
   filter(rtt_part_type == "Part_3") |>
   select(
-    period, provider_parent_org_code, provider_parent_name, provider_org_code,
-    provider_org_name, commissioner_parent_org_code, commissioner_parent_name,
-    commissioner_org_code, commissioner_org_name, treatment_function_code,
+    period:commissioner_org_name, treatment_function_code,
     treatment_function_name, total_all
   )
 
+# Remove the main RTT pathways from the original dataset
 data <- data |>
   filter(rtt_part_type != "Part_3")
 
+# Save both cleaned datasets
 saveRDS(starting, "cache/cleaned_starting.rds")
 saveRDS(data, "cache/cleaned_data.rds")
