7554c2cbbea96acabd1edac58e75ba9efe92f6ad
Nathaniel Victor <nathanielvictor2004@gmail.com>
2026-08-26 16:18:22 +0100

    feat: Created further tests in cleaning.r

--- test/cleaning.r
+++ test/cleaning.r
@@ -1,4 +1,5 @@
 library(testthat)
+library(dplyr)
 
 # Load cleaned datasets
 starting <- readRDS("cache/cleaned_starting.rds")
@@ -8,4 +9,20 @@ data <- readRDS("cache/cleaned_data.rds")
 test_that("Cleaned datasets contain no NA values", {
   expect_false(anyNA(starting))
   expect_false(anyNA(data))
+})
+
+# All patient counts should be non-negative
+test_that("All patient counts are non-negative", {
+  expect_true(all(select(data, gt_00_to_01_weeks_sum_1:total_all) >= 0))
+  expect_true(all(starting$total_all >= 0))
+})
+
+# All patient counts sum correctly
+test_that("All patient counts sum correctly", {
+  manual_total <-
+    rowSums(select(data, gt_00_to_01_weeks_sum_1:gt_104_weeks_sum_1))
+  manual_total_all <- data$total + data$patients_with_unknown_clock_start_date
+
+  expect_true(all(manual_total == data$total))
+  expect_true(all(manual_total_all == data$total_all))
 })
